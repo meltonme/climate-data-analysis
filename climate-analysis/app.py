2e85f1048be24e54a6993f0ad1b1ea82077e3a36
meltonme <mahanesme@gmail.com>
2024-03-11 10:49:04 -0400

    addressed an issue in the app.py file with the path

--- climate-analysis/app.py
+++ climate-analysis/app.py
@@ -1,29 +1,28 @@
 # Import the dependencies.
-import datetime as dt
 import numpy as np
-import pandas as pd
 import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt 
 
 from flask import Flask, jsonify
-from sqlalchemy import create_engine, func
-from sqlalchemy.orm import Session
-from sqlalchemy.ext.automap import automap_base
-
 #################################################
 # Database Setup
 #################################################
 
 
 # reflect an existing database into a new model
-engine = create_engine("sqlite:///..climate-data/hawaii.sqlite")
-# reflect the tables
+# this is the path that I used in the exploratory analysis
+engine = create_engine("sqlite:///../climate-data/hawaii.sqlite") 
 Base = automap_base()
+# reflect the tables
 Base.prepare(autoload_with=engine)
 # Save references to each table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
